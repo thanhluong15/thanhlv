a5bf31e8db9a9e32959dfc185b41a45833acd3c6
thanhluong15 <102784562+thanhluong15@users.noreply.github.com>
2023-05-24 21:16:47 +0700

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express, {Request, Response, NextFunction} from 'express'; 
+import bodyParser from 'body-parser'; 
+import { filterImageFromURL, deleteLocalFiles } from './util/util'; 
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // GET /filteredimage?image_url={{URL}} 
+  app.get("/filteredimage", async (req: Request, res: Response, next: NextFunction) => { 
+    // get URL image 
+    try { 
+      let absolutePath: string = await filterImageFromURL(req.query.image_url) as string; 
+      return res.status(200).sendFile(absolutePath, function (err) { 
+        console.log("File downloaded "); 
+        // delete file 
+        if (!err) { 
+          deleteLocalFiles([absolutePath]); 
+          console.log("File deleted "); 
+        } 
+      }); 
+    } catch (e) { 
+      return next(e); 
+    } 
+  });
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
